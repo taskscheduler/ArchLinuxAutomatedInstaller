9c09523696eab230a203d1c8a5e51ba2f9560c3d
CyOfficial <86893359+CyOfficial@users.noreply.github.com>
2022-04-01 21:10:28 +0000

    Potential fixes

--- archlinuxautomatedinstaller.sh
+++ archlinuxautomatedinstaller.sh
@@ -265,7 +265,7 @@ main () {
     echo "Setting the Root Password..."
     touch .passwd
     echo -e "$rootpass\n${rootpass}" > .passwd
-    arch-chroot /mnt "passwd root" < .passwd >/dev/null
+    arch-chroot /mnt /bin/bash -c "${1}" "passwd root" < .passwd >/dev/null
     rm .passwd
 
     # Creating a User with the Username that the User selected
@@ -276,7 +276,7 @@ main () {
     echo "Setting the User Password..."
     touch .passwd
     echo -e "$userpass\n${userpass}" > .passwd
-    arch-chroot /mnt "passwd ${username}" < .passwd >/dev/null
+    arch-chroot /mnt /bin/bash -c "${1}" "passwd ${username}" < .passwd >/dev/null
     rm .passwd
 
     # Adding the User Group 'wheel' to the sudoers list
