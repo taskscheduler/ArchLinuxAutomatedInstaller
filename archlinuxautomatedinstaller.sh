a3e65e65e5bd788ccde666bb4dbc1dd9bf273732
CyOfficial <86893359+CyOfficial@users.noreply.github.com>
2022-04-03 17:00:22 +0000

    XFCE Fixes

--- archlinuxautomatedinstaller.sh
+++ archlinuxautomatedinstaller.sh
@@ -366,7 +366,7 @@ main () {
         sleep 3s
 
         # Installing XFCE
-        arch-chroot /mnt pacman -S --noconfirm --needed xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk
+        arch-chroot /mnt pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
         arch-chroot /mnt systemctl enable lightdm
 
         # Notifying the User that the Installation is Complete
